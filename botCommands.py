379db410ece93b83f52095a9b171a3f5b869bb6b
Kevin <mrhallonline@gmail.com>
2019-12-24 01:12:07 -0500

    bot commands instead of client

---
+++ botCommands.py
@@ -0,0 +1,34 @@
+import os
+import discord
+import random
+
+from dotenv import load_dotenv
+from discord.ext import commands
+
+
+load_dotenv()
+token = os.getenv('DISCORD_TOKEN')
+
+bot = commands.Bot(command_prefix='!')
+
+@bot.event
+async def on_ready():
+    print(f'{bot.user.name} has connected to Discord')
+    
+@bot.command(name='99', help='Responds with a random quote from Brooklyn 99')
+async def nine_nine(ctx):
+    brooklyn_99_quotes = [
+        'I\'m the human form of the 💯 emoji.',
+        'Bingpot!',
+        (
+            'Cool, Cool, cool cool cool cool cool cool, '
+            'no doubt no doubt no doubt no doubt.'
+        ),
+    ]
+    
+    response = random.choice(brooklyn_99_quotes)
+    await ctx.send(response)
+
+bot.run(token)
+
+
