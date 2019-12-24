92c57ae6621c6eafa40dbd02ad5ec9289d8c76a3
Kevin <mrhallonline@gmail.com>
2019-12-24 01:38:57 -0500

    bot can creat channel based on user role

--- botCommands.py
+++ botCommands.py
@@ -29,6 +29,29 @@ async def nine_nine(ctx):
     response = random.choice(brooklyn_99_quotes)
     await ctx.send(response)
 
+@bot.command(name='roll_dice', help=' Simulates rolling dice.')
+async def roll(ctx, number_of_dice: int, number_of_sides: int):
+    dice = [
+        str(random.choice(range(1, number_of_sides+1)))
+        for _ in range (number_of_dice)
+    ]
+    await ctx.send(', '.join(dice))
+
+@bot.command(name='create_channel')
+@commands.has_role('admin')
+async def create_channel(ctx, channel_name='blue'):
+    guild = ctx.guild
+    existing_channel = discord.utils.get(guild.channels, name=channel_name)
+    if not existing_channel:
+        print(f'Creating a new channel: {channel_name}')
+        await guild.create_text_channel(channel_name)
+
+@bot.event
+async def on_command_error(ctx, error):
+    if isinstance(error, commands.errors.CheckFailure):
+        await ctx.send ('You do not have the correct role for this command.')
+
+
 bot.run(token)
 
 
